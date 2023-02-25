1aa9aa24a200cd809573c05a420de6e9215deb0a
Bers Yaskaev <semeikayaskaevih@gmail.com>
2023-02-25 14:01:57 +0500

    Добавил модель, роуты и контроллеры для отзывов

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,6 @@ router.use("/users", require("./users.route"));
 router.use("/chats", require("./chats.route"));
 router.use("/products", require("./products.route"));
 router.use("/cars", require("./cars.route"));
+router.use("/reviews", require("./reviews.route"));
 
 module.exports = router;
