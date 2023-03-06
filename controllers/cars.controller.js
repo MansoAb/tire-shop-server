f0de857cfacd848ecefdc573be0fd77c71ef57ec
Bers Yaskaev <semeikayaskaevih@gmail.com>
2023-03-06 14:57:43 +0500

    сделал фильтрацию по авто

--- controllers/cars.controller.js
+++ controllers/cars.controller.js
@@ -2,13 +2,59 @@ const Car = require("../models/Car.model");
 
 module.exports.carsController = {
   addCar: async (req, res) => {
-    const { carCompany, carModel } = req.body;
+    const { carCompany, carModel, tires, year, modification } = req.body;
     try {
       const car = await Car.create({
         carCompany,
         carModel,
+        tires,
+        year,
+        modification,
       });
       res.json(car);
+    } catch (error) {
+      res.json(error);
+    }
+  },
+  getCars: async (req, res) => {
+    try {
+      const cars = await Car.find({}, "carCompany").distinct("carCompany");
+      res.json(cars);
+    } catch (error) {
+      res.json(error);
+    }
+  },
+  getCarModel: async (req, res) => {
+    try {
+      const models = await Car.find(
+        { carCompany: req.params.id },
+        "carModel"
+      ).distinct("carModel");
+      res.json(models);
+    } catch (error) {
+      res.json(error);
+    }
+  },
+  getCarYear: async (req, res) => {
+    const { carCompany, carModel } = req.body;
+
+    try {
+      const years = await Car.find({ carCompany, carModel }, "year").distinct(
+        "year"
+      );
+      res.json(years);
+    } catch (error) {
+      res.json(error);
+    }
+  },
+  getCarModification: async (req, res) => {
+    const { carCompany, carModel, year } = req.body;
+    try {
+      const modifications = await Car.find(
+        { carCompany, carModel, year },
+        "modification"
+      ).distinct("modification");
+      res.json(modifications);
     } catch (error) {
       res.json(error);
     }
--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,4 +1,5 @@
 const Product = require("../models/Product.model");
+const Car = require("../models/Car.model");
 
 module.exports.productsController = {
   addProduct: async (req, res) => {
@@ -70,17 +71,19 @@ module.exports.productsController = {
     }
   },
   getProductsById: async (req, res) => {
-    const {arr} = req.body 
-    console.log("Mans")
+    const { arr } = req.body;
 
     try {
-      const products = []
-      for(let i = 0; i < arr.length; i++){
-        const prod = await Product.findById(arr[i].id)
-        products.push({count: arr[i].count, checked: arr[i].checked, _doc: prod})
+      const products = [];
+      for (let i = 0; i < arr.length; i++) {
+        const prod = await Product.findById(arr[i].id);
+        products.push({
+          count: arr[i].count,
+          checked: arr[i].checked,
+          _doc: prod,
+        });
       }
-      console.log(products)
-      return res.json(products)
+      return res.json(products);
     } catch (error) {
       return res.json(Error);
     }
@@ -90,127 +93,38 @@ module.exports.productsController = {
     const obj = req.body.tireArray[0];
 
     try {
-      const query = {};
-      for (const key in obj) {
-        if (obj[key] !== false && obj[key] !== 0) {
-          query[key] = obj[key];
+      if (obj.carCompany !== 0) {
+        const query = {};
+        for (const key in obj) {
+          if (obj[key] !== false && obj[key] !== 0) {
+            query[key] = obj[key];
+          }
         }
+
+        const products = await Car.find(query, "tires")
+          .populate("tires")
+          .lean();
+
+        const filterProd = products
+          .map((car) => car.tires)
+          .flat()
+          .filter((obj, index, arr) => {
+            return arr.findIndex((elem) => elem._id === obj._id) === index;
+          });
+
+        res.json(filterProd);
+      } else {
+        const query = {};
+        for (const key in obj) {
+          if (obj[key] !== false && obj[key] !== 0) {
+            query[key] = obj[key];
+          }
+        }
+        const products = await Product.find(query);
+        res.json(products);
       }
-      const products = await Product.find(query);
-      res.json(products);
     } catch (error) {
       res.json(error);
     }
   },
-
-  // sendEmail: async (req, res) => {
-  //   const { name, lastname, number, email, product, volume, sum } = req.body;
-  //   try {
-  //     const transporter = nodemailer.createTransport({
-  //       host: "smtp.gmail.com",
-  //       port: 465,
-  //       service: "gmail",
-  //       auth: {
-  //         user: process.env.EMAIL,
-  //         pass: process.env.PASSWORD,
-  //       },
-  //     });
-
-  //     const mailOptions = {
-  //       from: "tagvu86@gmail.com",
-  //       to: "tagvu86@gmail.com",
-  //       subject: "Заказ.",
-  //       text: `
-  //         Имя заказчика: ${name},
-  //         Фамилия заказчика: ${lastname},
-  //         Номер телефона заказчика: ${number},
-  //         Email заказчика: ${email},
-  //         Материал: ${product},
-  //         Объем: ${volume} тонн,
-  //         Сумма заказа: ${sum}
-
-  //       `,
-  //     };
-
-  //     const mailClientOptions = {
-  //       from: "tagvu86@gmail.com",
-  //       to: email,
-  //       subject: "Заказ.",
-  //       text: `
-  //         Ваш заказ "${product}" объемом ${volume} тонн на сумму ${sum} принят к рассмотрению.
-
-  //       `,
-  //     };
-
-  //     await transporter.sendMail(mailOptions);
-  //     await transporter.sendMail(mailClientOptions);
-
-  //     return res.json("Форма отправлена.");
-  //   } catch (error) {
-  //     return res.json(error);
-  //   }
-  // },
-
-  // autocompleteProducts: async (req, res) => {
-  //   const { searchProduct } = req.body;
-
-  //   try {
-  //     if (searchProduct !== "") {
-  //       const products = await Product.find({
-  //         productName: new RegExp(`${searchProduct}`, "i"),
-  //       });
-  //       if (products.length !== 0) {
-  //         return res.json(products);
-  //       }
-  //     }
-  //     return res.json({ error: "По вашему запросу ничего не найдено..." });
-  //   } catch (error) {
-  //     return res.json(error);
-  //   }
-  // },
-  // sendForm: async (req, res) => {
-  //   const { name, number, email, text } = req.body;
-  //   try {
-  //     const transporter = nodemailer.createTransport({
-  //       host: "smtp.gmail.com",
-  //       port: 465,
-  //       service: "gmail",
-  //       auth: {
-  //         user: "tagvu86@gmail.com",
-  //         pass: "iqboixmopesruuie",
-  //       },
-  //     });
-
-  //     const mailOptions = {
-  //       from: "tagvu86@gmail.com",
-  //       to: "tagvu86@gmail.com",
-  //       subject: "Связь клиента.",
-  //       text: `
-  //         Имя: ${name},
-  //         Номер: ${number},
-  //         Email: ${email},
-  //         Сообщение:
-  //         ${text}
-
-  //       `,
-  //     };
-
-  //     const mailClientOptions = {
-  //       from: "tagvu86@gmail.com",
-  //       to: email,
-  //       subject: "Спасибо за связь.",
-  //       text: `
-  //         Ваше сообщение отправлено админу, скоро он свяжется с вами.
-
-  //       `,
-  //     };
-
-  //     await transporter.sendMail(mailOptions);
-  //     await transporter.sendMail(mailClientOptions);
-
-  //     return res.json("Форма отправлена.");
-  //   } catch (error) {
-  //     return res.json(error);
-  //   }
-  // },
 };
